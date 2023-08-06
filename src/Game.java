0cb33564fc5d3f1445f0a01b198d7261e35b668c
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2023-08-06 16:35:14 +0600

    Meraj_GameFile

---
+++ src/Game.java
@@ -0,0 +1,49 @@
+
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Game {
+
+    public static ImageIcon image = new ImageIcon("DraftGame/src/main/Char/Front.png");
+    static final int screeenWidth = 768, screenHeight = 620;
+    public static JLabel cat, plank, river;
+    public static JFrame frame;
+
+    public static boolean game;
+
+    public static void main(String[] args) {
+        new FirstPage();
+    }
+
+    public static class GameWindow {
+
+        public GameWindow() {
+            frame = new JFrame();
+
+            cat = new JLabel();
+            cat.setBounds(0, 0, 52, 52);
+
+            frame.add(cat);
+
+            cat.setIcon(image);
+            frame.addKeyListener(new Keys());
+
+            frame.setPreferredSize(new Dimension(screeenWidth, screenHeight));
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().setBackground(Color.BLACK);
+
+            frame.setLayout(null);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+        }
+    }
+}
