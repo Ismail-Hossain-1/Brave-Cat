de2b62915a53ddd9f6b65dd02e15712989dd99eb
Ismail-Hossain-1 <107604527+Ismail-Hossain-1@users.noreply.github.com>
2023-08-07 16:25:55 +0600

    Update Game.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,46 +1,53 @@
-
 package main;
 
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class Game {
 
-    public static ImageIcon image = new ImageIcon("Brave-Cat/src/main/Char/Front.png");
-    static final int screeenWidth = 768, screenHeight = 620;
-    public static JLabel cat, plank, river;
+
+
+   public static ImageIcon image= new ImageIcon("Brave-Cat/src/main/Char/Front.png");
+   static final int screeenWidth= 768, screenHeight = 620;
+   public static JLabel cat, plank, river;
     public static JFrame frame;
 
     public static boolean game;
 
     public static void main(String[] args) {
-        new FirstPage();
+
+       new FirstPage();
     }
+  public static class GameWindow{
 
-    public static class GameWindow {
-
-        public GameWindow() {
+      public  GameWindow(){
             frame = new JFrame();
+            frame.setIconImage(image.getImage());
+            frame.setTitle("Brave Cat");
+
 
             cat = new JLabel();
             cat.setBounds(0, 0, 52, 52);
+           // cat.setLocation(0, 0);
+
 
             frame.add(cat);
 
+            frame.setLayout(null);
             cat.setIcon(image);
             frame.addKeyListener(new Keys());
 
+
             frame.setPreferredSize(new Dimension(screeenWidth, screenHeight));
+
 
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.getContentPane().setBackground(Color.BLACK);
 
-            frame.setLayout(null);
             frame.pack();
+
+
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
 
